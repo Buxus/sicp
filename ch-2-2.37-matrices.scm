b4704f5690182d2f6a84493265de145a841ff8b4
steve <shuysman@gmail.com>
2017-12-05 03:06:09 -0500

    Refactored matrix-*-vector, added matrix-*-matrix

--- ch-2-2.37-matrices.scm
+++ ch-2-2.37-matrices.scm
@@ -19,18 +19,50 @@
 (dot-product v w)     ; returns the sum >_i v_i w_i
 (matrix-*-vector m v) ; returns the vector t,
                       ; where t_i = >_j m_(ij) v_j
-(matrix-*-matrix m n)
-		; returns the matrix p,
-	      	; where p_(ij) = >_k m_(ik) n_(kj)
-(transpose m)   ; returns the matrix n,
-    		; where n_(ij) = m_(ji)
+(matrix-*-matrix m n) ; returns the matrix p,
+                      ; where p_(ij) = >_k m_(ik) n_(kj)
+(transpose m)         ; returns the matrix n,
+		      ; where n_(ij) = m_(ji)
 
-(define test-matrix (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+	    (accumulate-n op init (map cdr seqs)))))
+
+(define test-matrix (list (list 1 2 3 4) (list 4 5 6 6) (list 7 8 9 9)))
   
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map (lambra (x) * v x) m))
+  (map (lambda (row)
+	;; (accumulate + 0 (map * v w)))
+	 (dot-product row v))
+       m))
+
+(matrix-*-vector test-matrix (list 1 2 3 4))
+(5 11)
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) m)))
 
 
+(transpose test-matrix)
+((1 4 7) (2 5 8) (3 6 9) (4 6 9))
+
+(define test-matrix (list (list 1 2) (list 3 4)))
+(define test-matrix2 (list (list 1 2 3) (list 3 4 5)))
+
+(matrix-*-matrix test-matrix test-matrix2)
+((7 10 13) (15 22 29))
